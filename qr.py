8ea0f355cb33a568cefe66658c9c2d081ad15d7a
Santiago2021git <89594009+Santiago2021git@users.noreply.github.com>
2026-04-13 17:46:00 -0500

    Modify QR code script with empty URL and filename
    Updated QR code generation script to use empty URL and filename.

--- qr.py
+++ qr.py
@@ -1,6 +1,6 @@
 import qrcode #Instalar libreria en consola pip install qrcode
 
-url = "https://moto-tech.vercel.app/" #link o url para generar el codigo QR
+url = "" #link o url para generar el codigo QR
 qr = qrcode.QRCode(
     version=1,
     box_size=25,
@@ -11,5 +11,5 @@ qr.add_data(url)
 qr.make(fit=True)
 
 imagen= qr.make_image()
-imagen.save("Moto-Tech.jpg") # Nombre que llevara el codigo QR
+imagen.save("") # Nombre que llevara el codigo QR
 
